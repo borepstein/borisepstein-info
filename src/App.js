3b935dcac65c33665d389f6d2e94a3301eab2694
Boris Epstein <borepstein@gmail.com>
2020-07-28 18:22:55 +0000

    Initial checkin

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://i.postimg.cc/mDMYbbp5/me20110905.jpg" >
+		</img>
+		
+		<p>Links
+		</p>
+
+		<a href="https://peakd.com/@borepstein/posts">Blog</a>
+		<a href="https://gab.com/borepstein">Gab</a>
+		<a href="mailto:brosi.epstein@protonmail.com">E-Mail</a>
+		
+      </header>
+    </div>
+  );
+}
+
+export default App;
