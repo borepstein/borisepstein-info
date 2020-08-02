eb9bb11d28d15ff91e2e9f719c9740f45966cd21
Boris Epstein <borepstein@gmail.com>
2020-08-02 01:08:12 +0000

    Added clock on top

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import $ from 'jquery';
+import Clock from './Clock';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div class="App">
+      <header class="App-header">
+		<div class="timer-row">
+			<Clock />
+		</div>
 		<div class="row">
-			<div class="column">
+			<div class="column1">
         		<img src="https://i.postimg.cc/mDMYbbp5/me20110905.jpg" >
 				</img>
 			</div>
-			<div class="column">
-				<p>Links
-				</p>
+			<div class="column2">
+				<p>Worthy of a look</p>
+			</div>
+			<div class="column3">
+				<div class="Left-Alligned-Block">
+					<p>Links
+					</p>
 
-				<ul>
-				<li><a href="https://peakd.com/@borepstein/posts">Blog</a></li>
-				<li><a href="https://gab.com/borepstein">Gab</a></li>
-				<li><a href="mailto:borisepstein@protonmail.com">E-Mail</a></li>
-				</ul>
+					<ul>
+					<li><a href="https://peakd.com/@borepstein/posts">Blog</a></li>
+					<li><a href="https://gab.com/borepstein">Gab</a></li>
+					<li><a href="mailto:borisepstein@protonmail.com">E-Mail</a></li>
+					</ul>
+				</div>
 			</div>
 		</div>
       </header>
