34f5b1d413519b91e7db81e4fb7da923b34aefba
Boris Epstein <borepstein@gmail.com>
2021-09-03 20:50:40 -0400

    "class" to "className" conversion.

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ import HiveFeed from './HiveFeed';
 
 function App() {
   return (
-    <div class="App">
-      <header class="App-header">
-		<div class="timer-row">
+    <div className="App">
+      <header className="App-header">
+		<div className="timer-row">
 			Zulu time: <Clock />
 		</div>
-		<div class="row">
-			<div class="column1">
+		<div className="row">
+			<div className="column1">
         		<img src="https://i.postimg.cc/mDMYbbp5/me20110905.jpg" 
                          alt="Boris">
 				</img>
 				<p><i>Boris</i></p>
 				<p><i>Boston area, 2011</i></p>
 			</div>
-			<div class="column2">
+			<div className="column2">
 				<p>Worthy of a look</p>
 				<HiveFeed />
 			</div>
-			<div class="column3">
+			<div className="column3">
 				<div class="Left-Alligned-Block">
 					<p>Links
 					</p>
