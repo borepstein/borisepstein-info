70e5227f06985ba61a260780634a5dcfbf40c818
borepstein <borepstein@gmail.com>
2021-09-28 23:38:07 -0400

    Emergency fix. Got components out of App()

--- src/App.js
+++ src/App.js
@@ -3,60 +3,36 @@ import Clock from './Clock';
 import './App.css';
 import HiveFeed from './HiveFeed';
 
-function App() {
-  return (
-<<<<<<< HEAD
-    <div class="App">
-	<div class="App-header">
-            Zulu time: <Clock />
-	</div>
+function PersonalIntro() {
+    return(
+        <div>
+            <img src="https://i.postimg.cc/mDMYbbp5/me20110905.jpg" 
+                          alt="Boris">
+            </img>
+	    <p><i>Boris</i></p>
+            <p><i>Boston area, 2011</i></p>
+        </div>
+            );
+}
 
-      
-        <div class="row">
-			<div class="column1">
-=======
-    <div className="App">
-      <header className="App-header">
-		<div className="timer-row">
-			Zulu time: <Clock />
-		</div>
-		<div className="row">
-			<div className="column1">
->>>>>>> 34f5b1d413519b91e7db81e4fb7da923b34aefba
-        		<img src="https://i.postimg.cc/mDMYbbp5/me20110905.jpg" 
-                         alt="Boris">
-				</img>
-				<p><i>Boris</i></p>
-				<p><i>Boston area, 2011</i></p>
-			</div>
-<<<<<<< HEAD
-			<div class="column2">
-				<p>Recent blog entries</p>
-=======
-			<div className="column2">
-				<p>Worthy of a look</p>
->>>>>>> 34f5b1d413519b91e7db81e4fb7da923b34aefba
-				<HiveFeed />
-			</div>
-			<div className="column3">
-				<div class="Left-Alligned-Block">
-					<p>Links
+function StaticExternalLinks() {
+    return(
+            <div>
+            				<p>Links
 					</p>
 
 					<ul>
-<<<<<<< HEAD
+
 					<li><a href="https://peakd.com/@borepstein/posts" target="_blank">Blog</a></li>
 					<li><a href="https://gab.com/borepstein" target="_blank">Gab</a></li>
 					<li><a href="https://gettr.com/user/borepstein" target="_blank">Gettr</a></li>
 					<li><a href="https://www.facebook.com/borepstein" target="_blank">Facebook</a></li>
 					<li><a href="https://www.linkedin.com/in/borepstein/" target="_blank">LinkedIn</a></li>
-=======
 					<li><a href="https://peakd.com/@borepstein/posts" target="_blank" rel="noopener noreferrer">Blog</a></li>
 					<li><a href="https://gab.com/borepstein" target="_blank" rel="noopener noreferrer">Gab</a></li>
 					<li><a href="https://parler.com/profile/Borepstein/posts" target="_blank" rel="noopener noreferrer">Parler</a></li>
 					<li><a href="https://www.facebook.com/borepstein" target="_blank" rel="noopener noreferrer">Facebook</a></li>
 					<li><a href="https://www.linkedin.com/in/borepstein/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
->>>>>>> 34f5b1d413519b91e7db81e4fb7da923b34aefba
 					</ul>
 					
 					<p>Contact</p>
@@ -64,9 +40,36 @@ function App() {
 					<ul>
 					<li><a href="mailto:borisepstein@protonmail.com">E-Mail</a></li>
 					</ul>
+            </div>
+            );
+}
+
+function App() {
+  return (
+                        
+    <div className="App">
+      <header className="App-header">
+		<div className="timer-row">
+			Zulu time: <Clock />
+		</div>
+       </header>
+		<div className="row">
+			<div className="column1">
+
+                        <PersonalIntro />
+			</div>
+
+			<div className="column2">
+				<p>Worthy of a look</p>
+
+				<HiveFeed />
+			</div>
+			<div className="column3">
+				<div class="Left-Alligned-Block">
+                                <StaticExternalLinks />
 				</div>
 			</div>
-        </div>
+                </div>
       
     </div>
   );
